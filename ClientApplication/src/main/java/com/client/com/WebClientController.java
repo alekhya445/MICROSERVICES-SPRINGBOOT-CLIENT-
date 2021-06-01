c07543871e5c812609e3b53c7ea0aa97c942201b
alekhya445 <69209236+alekhya445@users.noreply.github.com>
2021-06-01 14:50:35 +0530

    Update WebClientController.java

--- ClientApplication/src/main/java/com/client/com/WebClientController.java
+++ ClientApplication/src/main/java/com/client/com/WebClientController.java
@@ -1,10 +1,14 @@
 package com.client.com;
 
+import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,7 +17,6 @@ public class WebClientController {
 
 		@Autowired
 		private RestTemplate restTemplate;
-		
 		
 		@GetMapping("/patients_record")
 		public List<Object> getAllPatients(){
@@ -26,5 +29,46 @@ public class WebClientController {
 		}
 		
 		
+		@GetMapping("/patients_record/{id}")
+		public List<Object> getAllPatients(@PathVariable int id){
+			
+			
+			
+			String URL= "http://localhost:2222/Patients/patients/" + id;
+			Object[] objects = restTemplate.getForObject(URL, Object[].class);
+			return Arrays.asList(objects);
+		}
+		
+		@GetMapping("/patients_record2/{name}")
+		public List<Object> getAllPatients(@PathVariable String name){
+			
+			
+			
+			String URL= "http://localhost:2222/Patients/retrive/" + name ;
+			Object[] objects = restTemplate.getForObject(URL, Object[].class);
+			return Arrays.asList(objects);
+		}
+		
+		
+		@PostMapping("/patients_record3")
+		public String getAllPatients(@RequestBody Patients patients){
+			
+			
+			
+			String URL= "http://localhost:2222/Patients/addPatients";
+			URI objects = restTemplate.postForLocation(URL, patients);
+			return "AddedPatientsSuccesfully";
+		}
+//		
 
-	}
+
+	
+//		@PutMapping("/patients_record")
+//		public List<Object> getAllPatients1() {
+//			String URL= "http://localhost:2222/Patients/addPatients";
+//			Object[] objects = restTemplate.getForObject(URL, Object[].class);
+//			return Arrays.asList(objects);
+//			
+//		}
+}
+
